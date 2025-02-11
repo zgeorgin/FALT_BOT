e55a4cafcfb2d7b6451ace9f9ccde9c26cb09b0c
zgeorgin <zgeorginn@gmail.com>
2025-02-11 11:19:28 +0300

    Better messaging with photos

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from handlers.registration_handler import reg_router
 from handlers.admin_interaction_handler import admin_router
 from handlers.laundry_handler import laundry_router
 from database.db import init_db
+from aiogram.methods import set_my_commands
+from aiogram.types import BotCommand, BotCommandScopeDefault
 import os
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -17,6 +19,11 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Начать работу с ботом')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
 dp = Dispatcher()
 async def main():
     init_db()
@@ -25,6 +32,7 @@ async def main():
     dp.include_router(admin_router)
     dp.include_router(laundry_router)
     await dp.start_polling(bot)
+    await set_commands()
     
     
 if __name__ == "__main__":
--- handlers/registration_handler.py
+++ handlers/registration_handler.py
@@ -16,7 +16,7 @@ class Registration(StatesGroup):
 
 @reg_router.callback_query(F.data == "registration")
 async def start_registration(call : CallbackQuery, state : FSMContext):
-    await call.message.edit_text("Отправьте фотографию из своего личного кабинета МФТИ, на которой видно вашу фотографию, имя и фамилию", reply_markup=get_cancel_kb())
+    await call.message.edit_caption(caption="Отправьте фотографию из своего личного кабинета МФТИ, на которой видно вашу фотографию, имя и фамилию", reply_markup=get_cancel_kb())
     await state.set_state(Registration.photo)
     
 @reg_router.message(Registration.photo)
